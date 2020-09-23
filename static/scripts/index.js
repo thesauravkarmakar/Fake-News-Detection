4b9015a2d4833879e22c7d67cd0f720cec56e20e
Aagam Jain <aagamjain113@gmail.com>
2020-09-23 23:24:20 +0530

    Updated Index and styles

---
+++ static/scripts/index.js
@@ -0,0 +1,14 @@
+function getInputValue(event) {
+    var inputVal = document.getElementById("searchtxt").value;
+    var real=document.getElementById("state");
+    // if (inputVal == 1) {
+    //     real.innerHTML='True!';
+    //     event.preventDefault();
+    // }
+    // else {
+    //     real.innerHTML='False!';
+    //     event.preventDefault();
+    // }
+}
+
+
