5180d5ea6ceccfdd574448e5ccda5ecf5da9f7d6
Aagam Jain <aagamjain113@gmail.com>
2020-09-28 13:11:23 +0530

    structured index and css

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -270,42 +270,6 @@
         $("#cmsgSubmit").removeClass().addClass(msgClasses).text(msg);
     }
 
-
-    /* Privacy Form */
-    $("#privacyForm").validator().on("submit", function(event) {
-    	if (event.isDefaultPrevented()) {
-            // handle the invalid form...
-            pformError();
-            psubmitMSG(false, "Please fill all fields!");
-        } else {
-            // everything looks good!
-            event.preventDefault();
-            psubmitForm();
-        }
-    });
-
-    function psubmitForm() {
-        // initiate variables with form content
-		var name = $("#pname").val();
-		var email = $("#pemail").val();
-        var select = $("#pselect").val();
-        var terms = $("#pterms").val();
-        
-        $.ajax({
-            type: "POST",
-            url: "php/privacyform-process.php",
-            data: "name=" + name + "&email=" + email + "&select=" + select + "&terms=" + terms, 
-            success: function(text) {
-                if (text == "success") {
-                    pformSuccess();
-                } else {
-                    pformError();
-                    psubmitMSG(false, text);
-                }
-            }
-        });
-	}
-
     function pformSuccess() {
         $("#privacyForm")[0].reset();
         psubmitMSG(true, "Request Submitted!");
@@ -325,22 +289,7 @@
             var msgClasses = "h3 text-center";
         }
         $("#pmsgSubmit").removeClass().addClass(msgClasses).text(msg);
-    }
-    
-
-    /* Back To Top Button */
-    // create the back to top button
-    $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
-    var amountScrolled = 700;
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > amountScrolled) {
-            $('a.back-to-top').fadeIn('500');
-        } else {
-            $('a.back-to-top').fadeOut('500');
-        }
-    });
-
-
+    }       
 	/* Removes Long Focus On Buttons */
 	$(".button, a, button").mouseup(function() {
 		$(this).blur();
